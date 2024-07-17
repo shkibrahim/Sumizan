f0c112936a4de1bca95f399a3015fbecd0138807
shkibrahim <sheikhibrahim026@gmail.com>
2024-07-18 01:50:54 +0500

    sumizan

--- src/Images/images.js
+++ src/Images/images.js
@@ -1,19 +1,19 @@
 const images = {
-    simpleLogo: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/simpleLogo.png?alt=media&token=e730c9cf-ecf4-4d80-850b-e244bc5269da',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Logo.png?alt=media&token=394fc035-4838-45fb-b345-d01dfbb552d8',
-    search: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/search.png?alt=media&token=75db97b1-0462-4b8e-bdc6-e8db3bce5957',//Icon
-    hamburg: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/3liness.png?alt=media&token=20e99a89-0ad9-4e20-9078-751887a8797b',//Icon
-    skewImg1: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/skewImgg.png?alt=media&token=1e03d3cb-7491-4d07-8823-a1be1f2aaf30',
-    skewImg2: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/skewImg1.png?alt=media&token=1faadb9e-aa65-4211-9365-58d0eea42110',
-    skewImg3: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/skewImg2.png?alt=media&token=cb1f23d4-4890-4719-9c8a-2a5e28624f3d',
+    simpleLogo: require('./simpleLogo.png'),
+    logo: require("./Logo.png"),
+    search: require('./search.png'),//Icon
+    hamburg: require('./3liness.png'),//Icon
+    skewImg1: require('./skewImgg.png'),
+    skewImg2: require("./skewImg1.png"),
+    skewImg3: require("./skewImg2.png"),
     browser: require("./Logo SVG/web_11264265.svg"),//Icon
     // browser: require("./browser.png"),//Icon
-    appDev:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/appDev.png?alt=media&token=31ae2f39-a6b3-48be-92ab-62b4c7d42a3a',
-    devSol: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/devSol.png?alt=media&token=96236fa2-9298-4d32-bc89-592a38eb47d8',
-    services1:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/services1.png?alt=media&token=1de5041b-36df-4f2a-83dd-7a9f872e2d1f',
-    services2: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/services2.png?alt=media&token=a80ec591-4355-4133-9fc5-69a065e5f041',
-    services3:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/services3.png?alt=media&token=64107d96-546d-44f1-8b51-7ac01ef96148',
-    services4:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/services4.png?alt=media&token=88bc2d45-6274-46e2-a015-6d979f770d5c',
+    appDev: require("./appDev.png"),
+    devSol: require("./devSol.png"),
+    services1: require("./services1.png"),
+    services2: require("./services2.png"),
+    services3: require("./services3.png"),
+    services4: require("./services4.png"),
     devSol1: require("./Sumizan Images/Devops/devSol1.jpg"),
     devSol2: require("./Sumizan Images/Devops/devSol2.jpg"),
     devSol3: require("./Sumizan Images/Devops/devSol3.jpg"),
@@ -56,19 +56,19 @@ const images = {
     cloud_4: require("./Sumizan Images/Cloud/cloud_4.jpg"),
     uiux: require("./uiux_icon.png"),//Icon
     more: require("./more.png"),//More Icon on Services Page
-    bulbs:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/3bulbs.png?alt=media&token=379fef16-4a6f-424b-b0fc-f41c55f6f314',
-    tickSearch: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/tickSearch.png?alt=media&token=811f9a2d-4aba-4e5a-8192-26c1bdcf02d2',
-    coinsBag: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/coinsBag.png?alt=media&token=dd3cf6bb-db24-4a22-a98c-0e3c2aa8864a',//Icon
-    skewImg4: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Vector%2024.png?alt=media&token=dd597f02-ec21-43d9-b8dd-c4a1c984fd46',
-    skewImg5: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Vector%2021.png?alt=media&token=76ade6c7-ed9a-4ed7-908c-fc5ad001bf95',
-    serviceSkew: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Vector%201.png?alt=media&token=242d56d3-2086-424b-97b9-c2ce805506ea',
-    portfolioSkew: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Vector%2027.png?alt=media&token=017e5c71-05eb-441c-afe2-8732b0d139ec',
-    contactSkew: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Vector%209.png?alt=media&token=108c6e1b-441f-4725-8626-bcc2f0fbebb0',
-    Artificial_Inteligence:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/Artificial%20Inteligence.png?alt=media&token=c4260dee-891c-458e-b583-2245ddf217e5',//Icon
-    Devops: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/devops_icon.png?alt=media&token=e9f8431d-9221-45dd-9698-48a02b90db0b',//Icon
-    appSkew: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/1.png?alt=media&token=5b60cfae-8781-49d1-8952-20c8c1f33c31',//Appdev Skew Image
-    webSkew: 'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/2.png?alt=media&token=d876ecd5-fa38-46eb-b393-3f6221f0070c',//Webdev Skew Image
-    devopSkew:'https://firebasestorage.googleapis.com/v0/b/crimereporter-730c1.appspot.com/o/3.png?alt=media&token=cf89a2f1-6bcb-4980-aa73-16e742dd2ce1',//Devops Skew Image
+    bulbs: require("./3bulbs.png"),
+    tickSearch: require("./tickSearch.png"),
+    coinsBag: require("./coinsBag.png"),//Icon
+    skewImg4: require("./Vector 24.png"),
+    skewImg5: require("./Vector 21.png"),
+    serviceSkew: require("./Vector 1.png"),
+    portfolioSkew: require("./Vector 27.png"),
+    contactSkew: require("./Vector 9.png"),
+    Artificial_Inteligence: require("./Artificial Inteligence.png"),//Icon
+    Devops: require("./devops_icon.png"),//Icon
+    appSkew: require("./1.png"),//Appdev Skew Image
+    webSkew: require("./2.png"),//Webdev Skew Image
+    devopSkew: require("./3.png"),//Devops Skew Image
 
 }
 
